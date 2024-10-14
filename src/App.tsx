a98a46c2b4942ada7a7cf6d2c5e929231eeb7515
Ramiro Graziano <graziano.ramiro@gmail.com>
2024-10-14 10:25:08 -0300

    feat(app): add header and main background

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { AppProviders } from "./App.providers"
 import { UsersList } from "@/modules/users/components/UsersList.component"
+import styles from "./App.module.scss"
 
 function App() {
   return (
     <AppProviders>
-      <main>
+      <header className={styles.header}>
+        <img src="https://cdn.prod.website-files.com/664501a053251deac9fb7060/664501a053251deac9fb70e8_keep-logo.svg" />
+      </header>
+
+      <main className={styles.mainContent}>
         <UsersList />
       </main>
     </AppProviders>
